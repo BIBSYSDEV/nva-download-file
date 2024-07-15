0cf705bdd536ba45e6bb5d842770a17c5f8fc4ad
Ketil Aasarød <ketil.aasarod@sikt.no>
2024-07-15 11:01:38 +0200

    fixed deserialization of resource owner no type field required

--- create-presigned-download-url/src/main/java/no/unit/nva/download/publication/file/publication/model/ResourceOwner.java
+++ create-presigned-download-url/src/main/java/no/unit/nva/download/publication/file/publication/model/ResourceOwner.java
@@ -1,10 +1,7 @@
 package no.unit.nva.download.publication.file.publication.model;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 import java.net.URI;
 
-@JsonTypeInfo(use = Id.NAME, property = "type")
 public record ResourceOwner(String owner, URI ownerAffiliation) {
 
 }
